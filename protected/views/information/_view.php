1b45b3c5d53e2c663d5960ce374cf4489f1e538e
Sarzz <sargismardanyan@gmail.com>
2015-05-16 14:02:57 +0400

    my app

---
+++ protected/views/information/_view.php
@@ -0,0 +1,33 @@
+<?php
+/* @var $this InformationController */
+/* @var $data Information */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('full_name')); ?>:</b>
+	<?php echo CHtml::encode($data->full_name); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('title')); ?>:</b>
+	<?php echo CHtml::encode($data->title); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('description')); ?>:</b>
+	<?php echo CHtml::encode($data->description); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('email')); ?>:</b>
+	<?php echo CHtml::encode($data->email); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('number')); ?>:</b>
+	<?php echo CHtml::encode($data->number); ?>
+	<br />
+
+
+</div>
